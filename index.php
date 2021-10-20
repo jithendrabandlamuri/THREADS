4f905581bdbbc05f1f10b3f00f5086c4b1603404
Bandlamuri Jithendra <65550122+jithendrabandlamuri@users.noreply.github.com>
2021-10-20 23:48:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,387 @@
+<?php   
+ session_start();  
+ $connect = mysqli_connect("localhost", "root", "", "safar");  
+ if(isset($_POST["add_to_cart"]))  
+ {  
+      if(isset($_SESSION["shopping_cart"]))  
+      {  
+           $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");  
+           if(!in_array($_GET["id"], $item_array_id))  
+           {  
+                $count = count($_SESSION["shopping_cart"]);  
+                $item_array = array(  
+                     'item_id'               =>     $_GET["id"],  
+                     'item_name'               =>     $_POST["hidden_name"],  
+                     'item_price'          =>     $_POST["hidden_price"],  
+                     'item_quantity'          =>     $_POST["quantity"]  
+                );  
+                $_SESSION["shopping_cart"][$count] = $item_array;  
+           }  
+           else  
+           {  
+                echo '<script>alert("Item Already Added")</script>';  
+                echo '<script>window.location="book.php"</script>';  
+           }  
+      }  
+      else  
+      {  
+           $item_array = array(  
+                'item_id'               =>     $_GET["id"],  
+                'item_name'               =>     $_POST["hidden_name"],  
+                'item_price'          =>     $_POST["hidden_price"],  
+                'item_quantity'          =>     $_POST["quantity"]  
+           );  
+           $_SESSION["shopping_cart"][0] = $item_array;  
+      }  
+ }  
+ if(isset($_GET["action"]))  
+ {  
+      if($_GET["action"] == "delete")  
+      {  
+           foreach($_SESSION["shopping_cart"] as $keys => $values)  
+           {  
+                if($values["item_id"] == $_GET["id"])  
+                {  
+                     unset($_SESSION["shopping_cart"][$keys]);  
+                     echo '<script>alert("Item Removed")</script>';  
+                     echo '<script>window.location="book.php"</script>';  
+                }  
+           }  
+      }  
+ }  
+ ?> 
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> Threads</title>
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-2018.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<style>
+.detail
+{
+  height:500px;
+}
+
+#pholder {
+  max-height: 100px;
+  max-width: 100px;
+}
+
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+}
+
+.wrapper {
+  text-align: center;
+}
+
+#darshan {
+  height: 500px;
+  padding-top: 50px;
+}
+
+#footer {
+  background-color: #e3f2fd;
+   height: 50px;
+    font-family: 'Verdana', sans-serif;
+    padding: 20px;
+}
+
+#photo {
+  max-height: 400px;
+  max-width: 250px;
+  padding-top: 20px;
+  padding-left: 20px;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 33%;
+  padding: 10 30px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 10px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+.logout{
+background-color: powderblue;
+  color: white;
+  border-radius: 10px;
+  
+  padding: 20px;
+  
+}
+</style>
+</head>
+<body style="background-color: powderblue;">
+<!--header-->
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <div class= "navbar-header">
+        <button type= "button" class="navbar-toggle" data-toggle= "collapse" data-target= "#bs-shuttle-navbar-collapse-1">
+          <span class= "sr-only"> Toggle navigation </span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+         </button>
+      <a class="navbar-brand" href="#"> Threads </a> 
+    </div>
+  <div class= "collapse navbar-collapse navbar-right" id="bs-shuttle-navbar-collapse-1">
+    <ul class= "nav navbar-nav">
+    <li type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Cart</li>
+    <li> <a class="logout" href="./homepage.php"> Logout </a> </li>
+    </ul>
+    </div>  
+  </div>
+</nav>
+<!--end of navbar-->
+<!--generic info-->
+<div class= "container-fluid">
+ <div class= "col-md-4" >
+  <div class= "panel panel-info" id="info" >
+      <div class= "panel-heading">
+        <h4 class= "panel-title" align="center"> Your Information </h4>
+      </div> 
+<?php 
+//$_SESSION['logged_user']=1;
+$server="localhost";
+$username="root";
+$password="";
+$db="safar";
+$t= $_SESSION['logged_user'];
+$conn = new mysqli($server,$username,$password,$db);
+if($conn->connect_error){
+    die("Connection failed".mysqli_connect_error());
+}
+  $result= $conn->query ("SELECT * FROM customer where cid = '$t'");
+  $row = $result->fetch_assoc();
+  $result1= $conn->query ("SELECT * FROM ride_details where cid = '$t' ORDER BY rid DESC LIMIT 1");
+ 
+  //echo mysqli_num_rows($result1);
+  /*if (mysqli_num_rows($result)>0)
+  {
+    $count= mysqli_num_rows($result);
+  }
+ while($row = $res1->fetch_assoc()) 
+{*/
+
+  $n = $row['name'];
+  $e = $row['email'];
+  $l = $row['type'];
+  $a = $row['amount'];
+
+//mysqli_close($conn);
+
+?>
+<div class="row">
+  <br>
+   
+      <img id="pholder" class= "center" src="./images/placeholder.png">
+      <br>
+      <br>
+      <table class = "table">
+      <tr>
+         <td align="center">Name: <?php echo "$n"; ?></td>
+      </tr>
+      
+      <tr>
+         <td align="center">E-mail: <?php echo "$e"; ?></td>
+      </tr>
+      
+      <tr>
+         <td align="center">Type: <?php echo "$l"; ?></td>
+      </tr>
+      <tr>
+         <td align="center">Amount: <?php echo "$a"; ?></td>
+      </tr>
+      
+   </table><br><br>
+   <center><button onclick="location.href='http://localhost/Threads/book1.php'" type="button" class="btn btn-success" value="Thread Products">Thread Products</button></center><br>
+    <center><button onclick="location.href='http://localhost/Threads/user.php'" type="button" class="btn btn-primary" value="exit">
+     Exit</button></center>
+   <br>
+   <br>
+   <div class= "wrapper">
+   </div>
+
+     
+    </div>
+  </div>
+</div>
+
+<div style="background-color: white; width: 800px; height: 650px; overflow: auto;">
+
+<div class="container" style="width:700px;">  
+                <h3 align="center">Welcome</h3><br />  
+                <?php  
+                $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
+                $result = mysqli_query($connect, $query);  
+                if(mysqli_num_rows($result) > 0)  
+                {  
+                     while($row = mysqli_fetch_array($result))  
+                     {  
+                ?>  
+                <div style="align-content: left;" class="col-md-4">  
+                     <form method="post" style="width: 200px;" action="book.php?action=add&id=<?php echo $row["id"]; ?>"> 
+
+                          <div style="border:1px solid black;float: left; width: 100%; padding:10 30px; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">  
+                            <br>
+                               <img style="width: 250px; height: 150px;" src="products/<?php echo $row["image"]; ?>" class="img-responsive" /><br />  
+                               <h4 style="height: 30px" class="text-info"><?php echo $row["name"]; ?></h4>  
+                               <h4 class="text-danger">₹ <?php echo $row["price"]; ?></h4>  
+                               <input type="text" name="quantity" class="form-control" value="1" />  
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
+                               <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
+                               <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
+                          </div>
+                     </form>  
+                </div>  
+                <?php  
+                     }  
+                }  
+                ?>   
+           </div>  
+           <br /> 
+
+  
+</div>
+</div>
+</div>
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class= "panel-heading">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+    <div style="clear:both"></div>  
+                <br />  
+                <center><h3>Order Details</h3></center>  
+                <div class="table-responsive">  
+                     <table class="table table-bordered">  
+                          <tr>  
+                               <th width="40%">Item Name</th>  
+                               <th width="10%">Quantity</th>  
+                               <th width="20%">Price</th>  
+                               <th width="15%">Total</th>  
+                               <th width="5%">Action</th>  
+                          </tr>  
+                          <?php   
+                          if(!empty($_SESSION["shopping_cart"]))  
+                          {  
+                               $total = 0;  
+                               foreach($_SESSION["shopping_cart"] as $keys => $values)  
+                               {  
+                          ?>  
+                          <tr>  
+                               <td><?php echo $values["item_name"]; ?></td>  
+                               <td><?php echo $values["item_quantity"]; ?></td>  
+                               <td>₹ <?php echo $values["item_price"]; ?></td>  
+                               <td>₹ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
+                               <td><a href="book.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
+                          </tr>  
+                          <?php  
+                                    $total = $total + ($values["item_quantity"] * $values["item_price"]);  
+                               }  
+                          ?>  
+                          <tr>  
+                               <td colspan="3" align="right">Total</td>  
+                               <td align="right">₹ <?php echo number_format($total, 2); ?></td>  
+                               <td></td>  
+                          </tr>  
+                          <?php  
+                          }  
+                          ?>  
+                     </table>  
+                </div> 
+      </div>
+      <div class="modal-footer">
+        <a href="book.php">Back</a> 
+      </div>
+    </div>
+  </div>
+</div> 
+
+</body>
+</html>
